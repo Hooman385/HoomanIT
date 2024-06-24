9c4ea44893d1f969c4167e637194a903c492af25
Hooman Mahdavi <Hooman.Mahdavi385@gmail.com>
2024-06-24 19:18:56 +0330

    change radio buttons to checkboxes to fix checked property not working

--- src/app/(admin)/edit-product/page.jsx
+++ src/app/(admin)/edit-product/page.jsx
@@ -1,12 +1,18 @@
 import EditProductTemplate from "@/components/templates/EditProductTemplate";
 import connectDB from "@/lib/connectDB";
 import Product from "@/models/Product";
+import { Toaster } from "react-hot-toast";
 
 async function EditProductPage({ searchParams }) {
   await connectDB();
   const product = await Product.findById(searchParams.id);
 
-  return <EditProductTemplate product={JSON.parse(JSON.stringify(product))} />;
+  return (
+    <>
+      <EditProductTemplate product={JSON.parse(JSON.stringify(product))} />
+      <Toaster />
+    </>
+  );
 }
 
 export default EditProductPage;
