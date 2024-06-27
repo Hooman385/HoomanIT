b39e334ae82f613fb389d6431115e99152a53064
Hooman Mahdavi <Hooman.Mahdavi385@gmail.com>
2024-06-27 20:08:30 +0330

    add better error handling in both client-side and api

--- src/app/(rest of the routes)/product/[productId]/page.jsx
+++ src/app/(rest of the routes)/product/[productId]/page.jsx
@@ -1,22 +1,40 @@
 import ProductTemplate from "@/components/templates/ProductTemplate";
 import Product from "@/models/Product";
 import connectDB from "@/lib/connectDB";
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params: { productId } }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_HOST_URL}api/getProduct/${productId}`
+  );
+  if (!response.ok) {
+    notFound();
+  }
+  const { title } = await response.json();
+
+  return {
+    title: title,
+  };
+}
 
 export async function generateStaticParams() {
   await connectDB();
   const products = await Product.find({});
   const posts = [];
-
   products.map((product) => {
     return [...posts, { productId: product.id }];
   });
-  return posts;
+  return posts.slice(0, 25);
 }
 
 async function page({ params: { productId } }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_HOST_URL}api/getProduct/${productId}`
   );
+
+  if (!response.ok) {
+    notFound();
+  }
   const result = await response.json();
 
   return <ProductTemplate details={result} />;
--- src/app/api/getProduct/[productId]/route.js
+++ src/app/api/getProduct/[productId]/route.js
@@ -2,17 +2,28 @@ import Product from "@/models/Product";
 import connectDB from "@/lib/connectDB";
 import { NextResponse } from "next/server";
 
+import { isValidObjectId } from "mongoose";
+
 export async function GET(req, { params }) {
-  try {
-    await connectDB();
-  } catch (error) {
-    return NextResponse.json({ error: "خطا در برقراری ارتباط با دیتابیس" });
-  }
+  await connectDB();
+
   const id = params.productId;
+  const isIdValid = isValidObjectId(id);
+
+  if (!isIdValid) {
+    return NextResponse.json(
+      { error: "آی‌دی واردشده نادرست است" },
+      { status: 400 }
+    );
+  }
+
   const product = await Product.findById(id);
 
   if (!product) {
-    return NextResponse.json({ error: "محصولی با این آیدی یافت نشد" });
+    return NextResponse.json(
+      { error: "محصولی با این آیدی یافت نشد" },
+      { status: 404 }
+    );
   }
 
   return NextResponse.json(product);
