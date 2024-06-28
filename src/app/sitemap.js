38490400752f6fbfb6365b83fb50b88195a8dab1
Hooman Mahdavi <Hooman.Mahdavi385@gmail.com>
2024-06-28 13:37:35 +0330

    query db for sitemap instead of fetch

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,27 @@
+// export default async function sitemap() {
+//   const response = await fetch(
+//     `${process.env.NEXT_PUBLIC_BASE_HOST_URL}api/getAllProductIDs`
+//   );
+//   const productIDs = await response.json();
+//   const productEntries = productIDs.map((id) => ({
+//     url: `${process.env.NEXT_PUBLIC_BASE_HOST_URL}product/${id}`,
+//   }));
+//   return [
+//     {
+//       url: `${process.env.NEXT_PUBLIC_BASE_HOST_URL}categories`,
+//     },
+//     ...productEntries,
+//   ];
+// }
+
+import connectDB from "@/lib/connectDB";
+import Product from "@/models/Product";
+
 export default async function sitemap() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_HOST_URL}api/getAllProductIDs`
-  );
-  const productIDs = await response.json();
+  await connectDB();
+  const products = await Product.find({});
+  const productIDs = products.map((product) => product.id);
+
   const productEntries = productIDs.map((id) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_HOST_URL}product/${id}`,
   }));
