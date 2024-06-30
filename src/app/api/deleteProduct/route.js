91b42b8c6cb87aa0be59f779ec37682d3a3a99d4
Hooman Mahdavi <Hooman.Mahdavi385@gmail.com>
2024-06-30 18:52:25 +0330

    fix index for products created after the deleted product

--- src/app/api/deleteProduct/route.js
+++ src/app/api/deleteProduct/route.js
@@ -7,6 +7,18 @@ export async function DELETE(req) {
   const url = new URL(req.url);
 
   await connectDB();
+  const productToDelete = await Product.findById({
+    _id: url.searchParams.get("id"),
+  });
+  if (!productToDelete) {
+    return NextResponse.json({ error: "product for deletion not found" });
+  }
+  const date = new Date(productToDelete.createdAt);
+  const products = await Product.find({ createdAt: { $gt: date } });
+  products.forEach(async (product) => {
+    product.index--;
+    await product.save();
+  });
   const result = await Product.deleteOne({ _id: url.searchParams.get("id") });
 
   if (result.deletedCount !== 1) {
